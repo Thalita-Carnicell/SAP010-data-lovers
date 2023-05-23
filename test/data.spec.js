5da28cb37df9e45036492348aced7eb7d9ec80f4
Aline Commit <128093293+Alinedev85@users.noreply.github.com>
2023-05-23 13:26:23 -0300

    arquivos refatorados

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,27 +1,28 @@
 // adotar cultura de testes - qualidade(métrica) confiança e tempo ( atingir objetivos de negócio) economia de tempo e dinheiro
 // 1* analise de requisitos- funcionalidades do projeto tipos de teste
-// 2* Plano de testes- (QA)ferramentas gastos de recursos 
-// 3* caso de testes- dados de entrada e saida 
+// 2* Plano de testes- (QA)ferramentas gastos de recursos
+// 3* caso de testes- dados de entrada e saida
 // 4* ambiente de teste- como e onde serão desenvolvidos ( fluxo )
 
-//escolher a unidade a ser testada// comportamento esperado//
+import { sortPokemonList } from '../src/data.js';
 
-import { filteredList } from '../';
 
-describe('filteredList', () => {
-  it('filtra a lista de Pokémon na ordem A-Z', () => {
-    const pokemon_List = [
-      { name: 'abomasnow' },
-      { name: 'abra' },
-      { name: 'absol' },
-    ];
+const mockPokemonList = [
+  { name: 'charizard' },
+  { name: 'darkrai' },
+  { name: 'xharizard' },
 
-    const filteredList = displayPokemonList(pokemon_List);
+];
 
-    expect(filteredList).toEqual([
-      { name: 'abomasnow' },
-      { name: 'abra' },
-      { name: 'absol' },
-    ]);
+describe('Ordenar lista de pokemons', () => {
+  it('deve classificar em ordem alfabetica crescente', () => {
+    const resultado = sortPokemonList(mockPokemonList, 'charizard', 'darkrai')
+    const esperado = 2;
+
+    expect(resultado.length).toEqual(esperado);
+
   });
+
 });
+
+
